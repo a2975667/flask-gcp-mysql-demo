e5ed17c077c78259fc98987bd74a2f4acec8d8f8
Benny Rochwerger <benny.rochwerger@redhat.com>
2023-11-12 09:27:03 +0200

    Create database and table

--- app/__init__.py
+++ app/__init__.py
@@ -1,25 +1,39 @@
 import os
+import sys
 import mysql.connector
 from flask import Flask
 
 def init_db_connector():
+    database = os.environ.get('MYSQL_DB')
     config = {
         'user' : os.environ.get('MYSQL_USER'),
         'password': os.environ.get('MYSQL_PASSWORD'),
-        'database': os.environ.get('MYSQL_DB'),
+        # 'database': os.environ.get('MYSQL_DB'),
         'host': os.environ.get('MYSQL_HOST'),
         'raise_on_warnings': True
     }
+    try:
+        cnx = mysql.connector.connect(**config)
+        cursor = cnx.cursor()
+        cursor.execute("CREATE DATABASE IF NOT EXISTS {}".format(database))
+        cursor.execute("USE {}".format(database))
+        cursor.execute("""
+              CREATE TABLE tasks (
+                    id int NOT NULL AUTO_INCREMENT,
+                    task varchar(255) NOT NULL,
+                    status char(30),
+                    PRIMARY KEY (id));
+                """)
+
+        cnx.close()
+    except Exception as e:
+        print(e)
+        
+    config['database'] = database
     cnx = mysql.connector.connect(**config)
     return cnx
 
 db = init_db_connector()
-
-# Test
-cursor = db.cursor()
-cursor.execute("SELECT * from tasks")
-print([x for x in cursor])
-cursor.close()
 
 app = Flask(__name__)
 from app import routes
